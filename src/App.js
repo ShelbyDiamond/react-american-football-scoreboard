79e89a7753179a96bc31e06b2579199bfc11e493
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-15 15:06:57 -0700

    updated 2 classes to have styled components

--- src/App.js
+++ src/App.js
@@ -2,40 +2,49 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import styled from "styled-components";
 
-const name1 = style.h2`
+const HomeTeam = styled.h2`
+  color: white;
+  text-transform: uppercase;
+  font-size: 3rem;
+  padding: 2rem;
+`;
+
+const BigGreenBox = styled.section`
+  background-color: rebeccapurple;
   width: 58%;
   margin: 10rem auto;
-  background-color: rebeccapurple;
   color: white;
   border-radius: 0.5rem;
   margin-top: 10rem;
   padding: 5rem;
   border: 4px solid #aaaaaa;
 `;
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
   return (
     <div className="container">
-      <section className="scoreboard">
+      <BigGreenBox>
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Russell</h2>
+            <HomeTeam>Russell</HomeTeam>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">05:18</div>
           <div className="away">
             <h2 className="away__name">Ruby</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
-      </section>
+      </BigGreenBox>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
